d6660e02cad69999603398276c7f0f8439bd0fc8
Chad Oakenfold <web@oakenfold.ca>
2017-05-05 13:22:50 -0700

    Express: Added bodyParser

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const config = require('./config');
 const port = config.port;
 
 // Express
+const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 
@@ -9,6 +10,8 @@ const app = express();
 //app.engine('html');
 //app.set('view engine', 'html');
 app.use('/public', express.static(path.join('..', 'public')));
+app.use(bodyParser.json()); // parses body of request as json and sticks it it in the req.body property
+app.use(bodyParser.urlencoded({extended: false})); // parses url-encoded data and attaches to req.body as string or arrays
 
 // Error Handlers
 app.use(function(err, req, res, next) {
